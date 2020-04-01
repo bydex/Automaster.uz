31ca57d643e8d0edf6f75313c69a16b4921a4144
bydex <erok_erok@mail.ru>
2020-04-01 17:32:07 +0600

    header

--- src/blocks/modules/big-banner/big-banner.js
+++ src/blocks/modules/big-banner/big-banner.js
@@ -2,28 +2,32 @@ import Swiper from "swiper";
 
 
 
-// let topSliderThumbs = new Swiper(".js-top-slider-bg", {
-//     spaceBetween: 30,
-//     slidesPerView: 3,
-//     freeMode: true,
-//     watchSlidesVisibility: true,
-//     watchSlidesProgress: true
-// });
-let topSliderThumbs = new Swiper(".js-top-slider-dots", {
-    spaceBetween: 30,
-    slidesPerView: 3,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true
+
+let topSliderBg = new Swiper(".js-top-slider-bg", {
+  speed: 1000,
+  slidesPerView: 1,
+  effect: "fade",
 });
-new Swiper(".js-top-slider", {
-    thumbs: {
-        swiper: topSliderThumbs
-    },
-    loop: true,
-    speed: 1000,
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false
-    }
+let topSliderThumbs = new Swiper(".js-top-slider-dots", {
+  spaceBetween: 30,
+  slidesPerView: 3,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+let topSlider = new Swiper(".js-top-slider", {
+  thumbs: {
+      swiper: topSliderThumbs
+  },
+  loop: true,
+  speed: 1000,
+  autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+  }
+});
+topSlider.on("slideChange", function() {
+  topSliderBg.slideTo(this.realIndex);
+});
+topSliderBg.on("slideChange", function() {
+  topSlider.slideTo(this.realIndex + 1);
 });
